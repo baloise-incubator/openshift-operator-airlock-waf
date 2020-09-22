4f5db82d4d23ae3a376794177f95f1826eef9107
Bobzin <michael.bobzin@baloise.ch>
2020-09-22 18:34:43 +0200

    creation of a basic mapping works

---
+++ baloise-airlock-waf-operator/src/main/java/com/baloise/waf/rest/api/client/beans/Data.java
@@ -0,0 +1,10 @@
+package com.baloise.waf.rest.api.client.beans;
+
+import java.util.Map;
+
+public class Data {
+
+public String type;
+public Attributes attributes;
+public Map<String, Object> additionalProperties;
+}
---
+++ baloise-airlock-waf-operator/src/main/java/com/baloise/waf/rest/api/client/beans/EntryPath.java
@@ -0,0 +1,9 @@
+package com.baloise.waf.rest.api.client.beans;
+
+import java.util.Map;
+
+public class EntryPath {
+
+public String value;
+public Map<String, Object> additionalProperties;
+}
